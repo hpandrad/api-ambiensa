5fcee726663972207c5335b0e155cc0eb6eb8dd5
Rocketking101 <josepaulofer98@gmail.com>
2022-12-07 22:32:02 -0500

    commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require("cors");
 const app = express();
 const controllers = require('./app/controllers');
-
 const PORT = process.env.PORT || 8080;
 
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
