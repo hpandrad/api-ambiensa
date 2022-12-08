e1045fc7624cb89f52e9fcc7e81d87f129b1bb8e
Rocketking101 <josepaulofer98@gmail.com>
2022-12-08 00:02:55 -0500

    commit

--- app/data_access/mysql.data_access.js
+++ app/data_access/mysql.data_access.js
@@ -1,20 +1,19 @@
 const db = require('../connections/mysql.connection');
-const queries = require('./queries');
 
 async function ConsultaEmpresas(){
-    let result = await db.query(queries.consultaEmpresas);
+    let result = await db.query('SELECT * FROM Empresa ORDER BY nombre');
 
     return result;
 }
 
 async function ConsultaEmpresaPorId(id){
-    let result = await db.query(queries.consultaEmpresaPorId, [id]);
+    let result = await db.query(`SELECT * FROM Empresa WHERE id = ${id}`);
 
     return result;
 }
 
 async function ValidaUsuario(usuario, clave){
-    let result = await db.query(queries.consultaUsuario, [usuario, clave]);
+    let result = await db.query(`SELECT * FROM Usuario WHERE usuario = '${usuario}' AND clave = '${clave}'`);
 
     return result;
 }
--- app/data_access/postgresql.data_access.js
+++ app/data_access/postgresql.data_access.js
@@ -1,20 +1,19 @@
 const db = require('../connections/postgresql.connection');
-const queries = require('./queries');
 
 async function ConsultaEmpresas(){
-    let result = await db.query(queries.consultaEmpresas);
+    let result = await db.query('SELECT * FROM Empresa ORDER BY nombre');
 
     return result;
 }
 
 async function ConsultaEmpresaPorId(id){
-    let result = await db.query(queries.consultaEmpresaPorId, [id]);
+    let result = await db.query('SELECT * FROM Empresa WHERE id = $1', [id]);
 
     return result;
 }
 
 async function ValidaUsuario(usuario, clave){
-    let result = await db.query(queries.consultaUsuario, [usuario, clave]);
+    let result = await db.query('SELECT * FROM Usuario WHERE usuario = $1 AND clave = $2', [usuario, clave]);
 
     return result;
 }
