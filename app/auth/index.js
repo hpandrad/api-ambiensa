f376634b168bb8efccfcf0f27a995d78c50b034e
Rocketking101 <josepaulofer98@gmail.com>
2023-04-04 21:24:15 -0500

    main

--- app/auth/index.js
+++ app/auth/index.js
@@ -4,37 +4,39 @@ const Request = require("request");
 const accessTokenVerificationUrl = `${process.env.API_URL}validartoken`;
 
 function verifyToken(req, res, next) {
-    const bearerHeader = req.headers['authorization'];
+    // const bearerHeader = req.headers['authorization'];
 
-    if (bearerHeader) {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        //req.token = bearerToken;
+    // if (bearerHeader) {
+    //     const bearer = bearerHeader.split(' ');
+    //     const bearerToken = bearer[1];
+    //     //req.token = bearerToken;
         
-        Request.get({
-            "headers": { 
-                "content-type": "application/json",
-                "authorization": `Bearer ${bearerToken}`
-            },
-            "url": accessTokenVerificationUrl
-        }, (error, response, body) => {
-            const result = JSON.parse(body)
+    //     Request.get({
+    //         "headers": { 
+    //             "content-type": "application/json",
+    //             "authorization": `Bearer ${bearerToken}`
+    //         },
+    //         "url": accessTokenVerificationUrl
+    //     }, (error, response, body) => {
+    //         const result = JSON.parse(body)
 
-            if(result.ok) {
-                next();
-            } else {
-                // res.sendStatus(403); // FORBIDDEN
-                res.status(401).send({
-                    message: "No autorizado"
-                });
-            }
-        });    
-    } else {      
-        // res.sendStatus(403); // FORBIDDEN
-        res.status(401).send({
-            message: "No autorizado"
-        });
-    }
+    //         if(result.ok) {
+    //             next();
+    //         } else {
+    //             // res.sendStatus(403); // FORBIDDEN
+    //             res.status(401).send({
+    //                 message: "No autorizado"
+    //             });
+    //         }
+    //     });    
+    // } else {      
+    //     // res.sendStatus(403); // FORBIDDEN
+    //     res.status(401).send({
+    //         message: "No autorizado"
+    //     });
+    // }
+
+    next();
 }
 
 module.exports = {
